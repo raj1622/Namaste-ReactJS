916522b8cccb76f334df9320082582b09e6f746b
Aryan Raj <aryanraj1622@gmail.com>
2025-02-23 20:39:47 +0530

    Food Junction

--- src/components/Body.js
+++ src/components/Body.js
@@ -24,7 +24,7 @@ const Body = () => {
     const data = await fetch(
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
-    // console.log(data);
+    // console.log(data)
     const json = await data.json();
     console.log(json);
 
